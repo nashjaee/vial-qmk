de7e4dc7149a1eaba6e3a128cb75b1f7a8234e12
Nema Ashjaee <nema.ashjaee@gmail.com>
2024-06-20 14:33:03 -0700

    Add Iris CE Vial keymap

---
+++ keyboards/keebio/iris_ce/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define EE_HANDS
+
+#define VIAL_KEYBOARD_UID {0xED, 0x2B, 0x08, 0xE8, 0xA4, 0xBA, 0x66, 0x14}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0} 
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
